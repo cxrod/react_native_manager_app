217e2feb4fc929d8119d3e98044fbf9b9feac523
crodriguez <crodriguez@bellbank.com>
2017-08-28 17:57:07 -0400

    lecture 137

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
@@ -12,11 +13,17 @@ const RouterComponent = () => {
                 </Scene>
                 <Scene key="main" >
                     <Scene 
-                        onRight={() => console.log('DERECHO!!!')}
+                        onRight={() => Actions.employeeCreate()}
                         rightTitle='Add'
                         key="employeeList" 
                         component={EmployeeList} 
                         title="Employee List" 
+                        initial
+                    />
+                    <Scene
+                        key="employeeCreate"
+                        title="Create Employee"
+                        component={EmployeeCreate}
                     />
                 </Scene>
             </Scene>
